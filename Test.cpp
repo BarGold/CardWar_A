654b562411fceccbcef8fadff7c1f103c06c18ba
BarGoldman <93201414+BarGoldman@users.noreply.github.com>
2023-03-25 10:39:36 +0000

    add test

--- Test.cpp
+++ Test.cpp
@@ -25,14 +25,15 @@ Game game1(p3,p4);
 Game game3(p7,p8);
 
 
-TEST_CASE("Game"){
+TEST_CASE("Game - SIMPEL TEST"){
 
-    // test for stacksize()
+    // test for stacksize() && cardesTaken() for new game
     CHECK(p1.stacksize() == 26);
     CHECK(p2.stacksize() == 26);
-    CHECK(p1.cardesTaken() == 0); // new game
-    CHECK(p2.cardesTaken() == 0); // new game
-    CHECK(p5.stacksize() == 0);
+    CHECK(p1.cardesTaken() == 0); 
+    CHECK(p2.cardesTaken() == 0); 
+
+    CHECK(p5.stacksize() == 0); // player that create but not play any Game 
 
     game.playTurn(); 
     
@@ -45,24 +46,19 @@ TEST_CASE("Game"){
     game.playTurn();
     CHECK(p1.stacksize() <= 24);
     CHECK(p2.stacksize() <= 24);
-    game.playTurn();
-    game.playTurn();
-    game.playTurn();
-    game.playTurn();
-    game.playTurn();
-    game.playTurn();
-
 }
 
-TEST_CASE("Game 1"){
-    // test for stacksize()
+TEST_CASE("Game 1 - CHECK_NOTHROW "){
+    // test for stacksize() && cardesTaken() for new game
     CHECK(p3.stacksize() == 26);
     CHECK(p4.stacksize() == 26);
-    CHECK(p3.cardesTaken() == 0); // new game
-    CHECK(p4.cardesTaken() == 0); // new game
+    CHECK(p3.cardesTaken() == 0);
+    CHECK(p4.cardesTaken() == 0); 
 
-    CHECK(p5.stacksize() == 0); // stay amount of cards left = 0 dont play
+    CHECK(p5.stacksize() == 0); 
     CHECK(p5.cardesTaken() == 0);
+
+    game1.playTurn();
 
     if(p3.cardesTaken() >= 2){ // player p3 is win 
        CHECK(p4.cardesTaken() == 0);
@@ -70,9 +66,18 @@ TEST_CASE("Game 1"){
     else{ // player p3 is loss
         CHECK(p3.cardesTaken() == 0);
     }
+    CHECK_NOTHROW(game1.printLastTurn());
     game1.playTurn();
     CHECK(p3.stacksize() <= 24);
     CHECK(p4.stacksize() <= 24);
+    for (int i=0;i<6;i++)
+    {
+        game1.playTurn();   
+    }
+    CHECK_NOTHROW(game1.playAll());
+    CHECK_NOTHROW(game1.printLog());
+    CHECK_NOTHROW(game1.printStats());
+
 }
 
 
@@ -81,13 +86,10 @@ TEST_CASE("Game 3")
     game3.playTurn();
     CHECK(p7.cardesTaken() != p8.cardesTaken()); // one of them need to win 
     CHECK(p5.stacksize() != 26); // p5 dont play a game
-    game3.playTurn();
-    game3.playTurn();
-    game3.playTurn();
-    game3.playTurn();
-    game3.playTurn();
-    game3.playTurn();
-    game3.playTurn();
+    for (int i=0;i<6;i++)
+    {
+        game3.playTurn();   
+    }
     if(p7.cardesTaken() != p8.cardesTaken()){
         if(p7.cardesTaken() > p8.cardesTaken()){
             CHECK(p7.cardesTaken() > p8.cardesTaken());
